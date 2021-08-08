14c3103557d5909ecd62728171e47fc9b0afd07f
Tanya Saxena <82470912+saxenatanya@users.noreply.github.com>
2021-08-08 08:27:30 -0400

    js to load current location using leaflet without classes

---
+++ script.js
@@ -0,0 +1,71 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+
+let map,mapEvent;
+// console.log(navigator);
+// console.log(navigator.geolocation);
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+        function (position) {
+            const { latitude } = position.coords;
+            const { longitude } = position.coords;
+            console.log(position);
+            console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+            const coords = [latitude,longitude];
+
+            var map = L.map('map').setView(coords, 13);
+
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
+
+            
+
+                map.on('click',function(mapE){
+                   mapEvent = mapE;
+                   form.classList.remove('hidden');
+                   inputDistance.focus();
+                });
+        },
+        function () {
+            alert("could not get the current location");
+        }
+    );
+
+    form.addEventListener('submit',function(e){
+e.preventDefault();
+
+//clear input fields
+inputDistance = inputDuration = inputCadence = inputElevation = '';
+
+
+//display marker 
+        const{lat,lng}= mapEvent.latlng;
+        L.marker([lat,lng]).addTo(map)
+    .bindPopup(L.bindPopup({
+        maxWidth:250,
+        minWidth:100,
+        autoClose:false,
+        closeOnClick:false,
+        className:'running-popup',
+    }))
+    .setPopupContent('workout')
+    .openPopup();
+    });
+}
+
+inputType.addEventListener('change',function(e){
+inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
